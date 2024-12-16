279b44e99cd8d32b7a44beeee0abc9239b2fc6e9
PudCheetah <wer212131@gmail.com>
2024-12-16 11:02:04 +0800

    Initial commit

--- app/src/main/java/com/example/mypratice_databinding_1/MainActivity.kt
+++ app/src/main/java/com/example/mypratice_databinding_1/MainActivity.kt
@@ -11,19 +11,24 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.mypratice_databinding_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-    private val viewModel by lazy {
-        ViewModelProvider(this).get(MyViewModel::class.java)
-    }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //先聲明binding,viewModel
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+
         setContentView(binding.root)
 
+
+        //將viewModel和布局文件中的變量關聯起來
         binding.myViewModel = viewModel
+        //使系統能感知 Activity 的生命週期，從而監聽 LiveData 的變化並自動更新 UI
         binding.lifecycleOwner = this
 
+        //按下按鈕後，會將ViewModel中的text_1值改為CCC
         binding.btnMain.setOnClickListener {
             viewModel.text_1.value = "CCC"
             Toast.makeText(this, "VM: ${viewModel.text_1.value}", Toast.LENGTH_SHORT).show()
